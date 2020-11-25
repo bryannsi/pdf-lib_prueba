88a7f21a1598844db4b7116ab3b3c10401ae85e7
Bryan Chavarría Hughes <bryannsi@users.noreply.github.com>
2020-11-25 17:57:24 -0600

    Update PdfDocument.js

--- PdfDocument.js
+++ PdfDocument.js
@@ -1,25 +1,49 @@
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
-const uri = "/var/www/html/pdf-lib_prueba/dod_character.pdf";
+const path = require("path");
+const file = "dod_character.pdf";
 
 const createPDF = async () => {
-  const formPdfBytes = fs.readFileSync(uri);
+  const formPdfBytes = fs.readFileSync(path.resolve(__dirname, `.\\${file}`));
   const pdfDoc = await PDFDocument.load(formPdfBytes);
   const form = pdfDoc.getForm();
-  const nameField = form.getTextField("CharacterName 2");
+
   const ageField = form.getTextField("Age");
   const weightField = form.getTextField("Weight");
 
+  const nameField = form.getTextField("CharacterName 2");
   nameField.setText("Bryan");
   ageField.setText("26");
   weightField.setText("83");
 
-  const pdfBytes = pdfDoc.save();
+  const fields = getAcroForm(form);
+  flatAcroForm(fields);
+
+  const pdfBytes = await pdfDoc.save();
   const name = `test${Date.now()}.pdf`;
 
   fs.writeFileSync(`./${name}`, pdfBytes);
 
   return pdfBytes;
+};
+
+const flatAcroForm = (fields) => {
+  fields.forEach((field) => {
+    //let type = field.constructor.name;
+    // let name = field.getName();
+    field.enableReadOnly();
+  });
+};
+
+const getAcroForm = (form) => {
+  const validTypes = ["PDFTextField"];
+  const fields = form.getFields();
+  let fieldList = [];
+  fields.forEach((field) => {
+    if (field && validTypes.includes(field.constructor.name))
+      fieldList.push(field);
+  });
+  return fieldList;
 };
 
 exports.createPDF = createPDF;
